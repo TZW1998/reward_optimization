35a6a5c704222394cee30e8eafadea62f18a07c4
TZW1998 <mcstzw@gmail.com>
2023-10-16 13:38:07 +0800

    2 gpus

--- config/base.py
+++ config/base.py
@@ -53,7 +53,7 @@ def get_config():
     sample.batch_size = 16
     # number of batches to sample per epoch. the total number of samples per epoch is `num_batches_per_epoch *
     # batch_size * num_gpus`.
-    sample.num_batches_per_epoch = 16
+    sample.num_batches_per_epoch = 8
 
     ###### Training ######
     config.train = train = ml_collections.ConfigDict()
@@ -73,7 +73,7 @@ def get_config():
     train.adam_epsilon = 1e-8
     # number of gradient accumulation steps. the effective batch size is `batch_size * num_gpus *
     # gradient_accumulation_steps`.
-    train.gradient_accumulation_steps = 32
+    train.gradient_accumulation_steps = 16
     # maximum gradient norm for gradient clipping.
     train.max_grad_norm = 1.0
     # number of inner epochs per outer epoch. each inner epoch is one iteration through the data collected during one
