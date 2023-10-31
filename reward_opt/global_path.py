0389143f1631490761c17025fa86602109cf7579
TZW1998 <mcstzw@gmail.com>
2023-10-31 17:51:36 +0800

    update common global_path

--- reward_opt/global_path.py
+++ reward_opt/global_path.py
@@ -1,17 +1,17 @@
 # Path to pretrained models
-SD_PRETRAINED_PATH = 'reward_opt/pretrained_models/sd.pth'
-CLIP_VIT_PRETRAINED_PATH = 'reward_opt/pretrained_models/clip_vit.pth'
+SD_PRETRAINED_PATH = '/home/zhiweitang/sdv1-5-full-diffuser'
+CLIP_VIT_PRETRAINED_PATH = '/home/zhiweitang/clip-vit-large-patch14'
 
 # Path to offline image data
 OFFLINE_IMAGE_PATH = {
-    "simple_animals": "reward_opt/data/simple_animals",
+    "simple_animals": "/home/zhiweitang/simple_animals_data",
 }
 
 
 # Path to offline reward data
 OFFLINE_REWARD_PATH = {
     "simple_animals": {
-        "aesthetic_score": "reward_opt/data/simple_animals/aesthetic.txt",
-        "jpeg_compressibility" : "reward_opt/data/simple_animals/jpeg_compressibility.txt",
+        "aesthetic_score": "/home/zhiweitang/simple_animals_reward/aesthetic.txt",
+        "jpeg_compressibility" : "/home/zhiweitang/simple_animals_reward/compressibility.txt",
     }
 }
