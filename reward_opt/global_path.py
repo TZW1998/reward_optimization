6e484a1c90870ce9b6dc2a0656cb403ffbf38edf
TZW1998 <mcstzw@gmail.com>
2023-11-09 18:04:16 +0800

    update global path for damo server

--- reward_opt/global_path.py
+++ reward_opt/global_path.py
@@ -1,36 +1,36 @@
 ## For SRIBD server
 #Path to pretrained models
-SD_PRETRAINED_PATH = '/home/zhiweitang/sdv1-5-full-diffuser'
-CLIP_VIT_PRETRAINED_PATH = '/home/zhiweitang/clip-vit-large-patch14'
-
-# Path to offline image data
-OFFLINE_IMAGE_PATH = {
-    "simple_animals": "/home/zhiweitang/simple_animals_data",
-}
-
-
-# Path to offline reward data
-OFFLINE_REWARD_PATH = {
-    "simple_animals": {
-        "aesthetic_score": "/home/zhiweitang/simple_animals_aesthetic_score.json",
-        "jpeg_compressibility" : "/home/zhiweitang/simple_animals_jpeg_compressibility.json",
-    }
-}
-
-# ## For DAMO server
-# SD_PRETRAINED_PATH = '/mnt/workspace/workgroup/tangzhiwei.tzw/sdv1-5-full-diffuser'
-# CLIP_VIT_PRETRAINED_PATH = '/mnt/workspace/workgroup/tangzhiwei.tzw/clip-vit-large-patch14'
+# SD_PRETRAINED_PATH = '/home/zhiweitang/sdv1-5-full-diffuser'
+# CLIP_VIT_PRETRAINED_PATH = '/home/zhiweitang/clip-vit-large-patch14'
 
 # # Path to offline image data
 # OFFLINE_IMAGE_PATH = {
-#     "simple_animals": "/mnt/workspace/workgroup/tangzhiwei.tzw/simple_animals_data",
+#     "simple_animals": "/home/zhiweitang/simple_animals_data",
 # }
 
 
 # # Path to offline reward data
 # OFFLINE_REWARD_PATH = {
 #     "simple_animals": {
-#         "aesthetic_score": "/mnt/workspace/workgroup/tangzhiwei.tzw/simple_animals_aesthetic_score.json",
-#         "jpeg_compressibility" : "/mnt/workspace/workgroup/tangzhiwei.tzw/simple_animals_jpeg_compressibility.json",
+#         "aesthetic_score": "/home/zhiweitang/simple_animals_aesthetic_score.json",
+#         "jpeg_compressibility" : "/home/zhiweitang/simple_animals_jpeg_compressibility.json",
 #     }
 # }
+
+# ## For DAMO server
+SD_PRETRAINED_PATH = '/mnt/workspace/workgroup/tangzhiwei.tzw/sdv1-5-full-diffuser'
+CLIP_VIT_PRETRAINED_PATH = '/mnt/workspace/workgroup/tangzhiwei.tzw/clip-vit-large-patch14'
+
+# Path to offline image data
+OFFLINE_IMAGE_PATH = {
+    "simple_animals": "/mnt/workspace/workgroup/tangzhiwei.tzw/simple_animals_data",
+}
+
+
+# Path to offline reward data
+OFFLINE_REWARD_PATH = {
+    "simple_animals": {
+        "aesthetic_score": "/mnt/workspace/workgroup/tangzhiwei.tzw/simple_animals_data_aesthetic_score.json",
+        "jpeg_compressibility" : "/mnt/workspace/workgroup/tangzhiwei.tzw/simple_animals_jpeg_data_compressibility.json",
+    }
+}
